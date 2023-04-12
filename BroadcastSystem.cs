ec38a62f45131167e83a920d3083521c4827f5ed
DaNoNeOfficial <daneonofficial@gmail.com>
2023-04-13 05:48:59 +1200

    updated to the latest version of Qurre

---
+++ BroadcastSystem.cs
@@ -0,0 +1,56 @@
+﻿using BroadcastSystem;
+using Qurre.API;
+using Qurre.API.Attributes;
+using Qurre.Events;
+using Qurre.Events.Structs;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+[PluginInit("BroadcastSystem", "DaNoNe", "1.0.0")]
+static class Plugin 
+{
+    private static List<string> bcMessages;
+    public static Random rnd = new Random();
+
+    [PluginEnable]
+    static internal void Enabled()
+    {
+        Configs.LoadReloadConfig();
+        Log.Info("Plugin Enabled");
+    }
+    [PluginDisable]
+    static internal void Disabled()
+    {
+        Log.Info("Plugin Disabled");
+    }
+
+    [EventMethod(PlayerEvents.Join)]
+    static internal void Join(JoinEvent ev)
+    {
+        Player player = ev.Player;
+        player.Client.Broadcast($"{Configs.joinPlayerText.Replace("%player%", player.UserInfomation.Nickname)}", (ushort) Configs.displayBroadcast);
+    }
+    [EventMethod(RoundEvents.Start)]
+    static internal async void RoundStart(RoundStartedEvent ev)
+    {
+        bcMessages = new List<string>(Configs.broadcastMessages);
+        await Task.Delay(Configs.startWaiting * 1000);
+        BroadcastTimingAsync();
+    }
+    static internal async void BroadcastTimingAsync()
+    {
+        while (!Round.Ended)
+        {
+            int randomValue = rnd.Next(0, bcMessages.Count - 1);
+            Map.Broadcast($"{bcMessages[randomValue]}", (ushort)Configs.displayBroadcast);
+            bcMessages.Remove(bcMessages[randomValue]);
+            if (bcMessages.Count == 0)
+            {
+                bcMessages = new List<string>(Configs.broadcastMessages);
+            }
+
+            await Task.Delay(Configs.nextBroadcast * 1000);
+        }
+    }
+}
---
+++ Configs.cs
@@ -0,0 +1,37 @@
+﻿using Qurre.API.Addons;
+using System.Collections.Generic;
+using Qurre.API.Attributes;
+
+namespace BroadcastSystem
+{
+    public static class Configs
+    {
+        internal static JsonConfig Config { get; private set; }
+        public static int startWaiting { get; private set; }
+        public static int nextBroadcast { get; private set; }
+        public static int displayBroadcast { get; private set; }
+        public static string joinPlayerText { get; private set; }
+        public static List<string> broadcastMessages { get; private set; }
+        [EventMethod(4001u, 0)]
+        internal static void LoadReloadConfig()
+        {
+            if (Config == null)
+            {
+                Config = new JsonConfig("BroadcastSystem");
+            }
+
+            startWaiting = Config.SafeGetValue("BroadcastStartWaiting", 30, "How many seconds after the start of the round will the messages start working?");
+            nextBroadcast = Config.SafeGetValue("BroadcastInterval", 120, "Interval between messages (in seconds)");
+            displayBroadcast = Config.SafeGetValue("BroadcastDisplay", 15, "How long will these messages be shown to the player?");
+            joinPlayerText = Config.SafeGetValue("JoinPlayerText", "<color=yellow>Hello <color=red>%player%</color> are you playing on the server</color>\n<b><color=yellow>My Server Name</color></b>", "Text for the player when he logged in to the server.");
+            broadcastMessages = Config.SafeGetValue("BroadcastMessages", new List<string>()
+            {
+                "<color=yellow>Hello</color>",
+                "<color=yellow>How are you</color>",
+                "<color=yellow>Join in my Discord</color>"
+            }, "Messages that will be sent at n intervals");
+
+            JsonConfig.UpdateFile();
+        }
+    }
+}
